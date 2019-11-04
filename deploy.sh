4b9ee5514eae3d83de538f887ff39b90c6d9659c
Henry <genrih.metlov@gmail.com>
2019-11-04 21:50:55 +0300

    Added deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# build
+npm run build
+
+# Add and push to git
+git add dist && git commit -m "Some changes"
+git subtree push --prefix dist origin gh-pages
+
+# https://medium.com/@Roli_Dori/deploy-vue-cli-3-project-to-github-pages-ebeda0705fbd
