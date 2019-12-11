dc98a2787805b4edb7cf9023a0cd2ccb67d4641f
Henry <genrih.metlov@gmail.com>
2019-12-12 00:13:04 +0300

    Add descriptions

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,7 +18,7 @@
                <a class="dropdown-item" href="#" 
                   v-for="m in modes" 
                   :key="m.key"
-                  @click="selectMode(m.key)">
+                  @click="selectMode(m)">
                   {{ m.title }}
                 </a>
             </div>
@@ -62,11 +62,31 @@
             <button class="btn btn-outline-primary" type="button" v-else disabled="true">Select algorithm</button>
           </li>
           <li class="nav-item" ><a class="nav-link" href="#" @click="clearBoard">Clear Board</a></li>
-          <li class="nav-item"><a class="nav-link"  href="#" @click="clearWalls">Clear Walls</a></li>
+          <!-- <li class="nav-item"><a class="nav-link"  href="#" @click="clearWalls">Clear Walls</a></li> -->
           <li class="nav-item"><a class="nav-link"  href="#" @click="clearSearchResult">Clear Search Result</a></li>
         </ul>
       </div>
     </nav>
+    <div>
+      <div class="gameModeDescription">
+        <ul style="display: block;">
+          <li><h5><mark>{{ currentGameMode.title }}</mark>. {{ currentGameMode.description }}.</h5></li>
+          <li><div class="cell start"></div>Start node</li>
+          <li><div class="cell finish"></div>Finish node</li>
+          <li><div class="cell visited"></div>Visited nodes</li>
+          <li><div class="cell unvisited"></div>Unvisited nodes</li>
+          <li><div class="cell shortest-path"></div>Shortest-path</li>
+          <li v-if="currentGameMode.key == 'simple'"><div class="cell wall"></div>Wall nodes</li>
+          <li v-if="currentGameMode.key == 'weight'"><div class="cell mountain"></div><div class="cell hill"></div>Mountain and Hills nodes</li>
+        </ul>
+      </div>
+      <div class="algDescription">
+        <h5 v-if="selectedAlgorithm.title != undefined" >
+          {{ selectedAlgorithm.description }}
+        </h5>
+        <h5 v-else> Pick an algorithm and visualize it! </h5>
+      </div>
+    </div>
   </header>
 </template>
 
@@ -75,14 +95,19 @@ export default {
   data() {
     return {
       selectedAlgorithm: {},
+
+      selectedGameMode: {},
+    
       algorithms: [
         { 
           title: "Dijkstra's Algorithm",
-          key: "dijkstra"
+          key: "dijkstra",
+          description: "Dijkstra's Algorithm is weighted and guarantees the shortest path!",
         },
         { 
           title: "A* Algorithm",
-          key: "astar"
+          key: "astar",
+          description: "A* Search is weighted and guarantees the shortest path!",
         },
       ],
 
@@ -100,12 +125,14 @@ export default {
       modes: [
         {
           title: "Simple",
-          key: "simple"
+          key: "simple",
+          description: "You can draw walls",
         },
 
         {
           title: "With weight",
-          key: "weight"
+          key: "weight",
+          description: "You can draw mountains and hills (weighted cells)",
         }
       ],
     }
@@ -120,8 +147,9 @@ export default {
       this.$emit('create-pattern', key);
     },
 
-    selectMode(key) {
-      this.$emit('change-mode', key);
+    selectMode(m) {
+      this.selectedGameMode = m;
+      this.$emit('change-mode', m.key);
     },
 
     runAlgorithm(key) {
@@ -140,8 +168,76 @@ export default {
       this.$emit('clear-search-result');
     },
   },
+
+  computed: {
+    currentGameMode() {
+      if (this.selectedGameMode.title == undefined) {
+        return this.modes[0];
+      } else {
+        return this.selectedGameMode;
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
+  .algDescription {
+    margin-bottom: 10px;
+  }
+
+  .gameModeDescription {
+    margin-top: 10px;
+  }
+
+  .gameModeDescription > ul li {
+    display: inline-block;
+    list-style: none;
+    padding: 0px 28px 0px 0px;
+  }
+
+  .cell {
+    width: 25px;
+    height: 25px;
+    display: inline-block;
+    position: relative;
+    top: 0.35em;
+    margin: 0px 0.42em;
+  }
+
+  .visited {
+    background-color:rgba(0, 190, 218, 0.75);
+  }
+
+  .unvisited{
+    background-color:white;
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(175, 216, 248);
+    border-image: initial;
+  }
+
+  .finish {
+    background-color: red;
+  }
+
+  .start {
+    background-color: green;
+  }
+
+  .shortest-path{
+    background-color: rgb(41, 173, 71);
+  }
+
+  .wall {
+    background-color: rgb(12, 53, 71);
+  }
+
+  .mountain {
+    background-color: rgb(107, 0, 0);
+  }
+
+  .hill {
+    background-color: rgb(242, 243, 155);
+  }
 </style>
